174636ef66f48efe6dbecf8066867c355780fb02
cinemaKH <kamimegane1123@gmail.com>
2020-08-05 21:44:47 +0900

    進捗3
    フェードインとか

---
+++ script2.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('body').fadeMover();
+});
+
+$("head").append('<style>.box { opacity: 0;-ms-filter:"alpha( opacity=0 )";filter: alpha( opacity=0 ); }</style>');
+$(function(){
+  $(".box").fadeMover();
+});
